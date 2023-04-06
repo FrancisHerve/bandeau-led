a6d139f24139d734ef28e45792e4182d147c3d34
FrancisHerve <128503272+FrancisHerve@users.noreply.github.com>
2023-04-06 15:37:13 +0200

    Update main.c
    modif avec intégration du SPI avec Init_SPI et le fonction mySPI_Thread

--- main.c
+++ main.c
@@ -40,13 +40,21 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "Board_LED.h"                  // ::Board Support:LED
+#include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
 
+#include "Driver_SPI.h"
 #ifdef _RTE_
 #include "RTE_Components.h"             // Component selection
 #endif
 #ifdef RTE_CMSIS_RTOS2                  // when RTE component CMSIS RTOS2 is used
 #include "cmsis_os2.h"                  // ::CMSIS:RTOS2
 #endif
+
+
+extern ARM_DRIVER_SPI Driver_SPI1;
+void mySPI_Thread (void const *argument);                             // thread function
+osThreadId tid_mySPI_Thread;                                          // thread id
+osThreadDef (mySPI_Thread, osPriorityNormal, 1, 0);                   // thread object
 
 #ifdef RTE_CMSIS_RTOS2_RTX5
 /**
@@ -86,6 +94,79 @@ uint32_t HAL_GetTick (void) {
 /* Private function prototypes -----------------------------------------------*/
 static void SystemClock_Config(void);
 static void Error_Handler(void);
+void Init_SPI(void);
+
+
+
+
+
+//fonction de CB lancee si Event T ou R
+void mySPI_callback(uint32_t event)
+{
+	switch (event) {
+		
+		
+		case ARM_SPI_EVENT_TRANSFER_COMPLETE  : 	 osSignalSet(tid_mySPI_Thread, 0x01);
+																							break;
+		
+		default : break;
+	}
+}
+
+void Init_SPI(void){
+	Driver_SPI1.Initialize(mySPI_callback);
+	Driver_SPI1.PowerControl(ARM_POWER_FULL);
+	Driver_SPI1.Control(ARM_SPI_MODE_MASTER | 
+											ARM_SPI_CPOL1_CPHA1 | 
+//											ARM_SPI_MSB_LSB | 
+											ARM_SPI_SS_MASTER_UNUSED |
+											ARM_SPI_DATA_BITS(8), 1000000);
+	Driver_SPI1.Control(ARM_SPI_CONTROL_SS, ARM_SPI_SS_INACTIVE);
+}
+
+
+void mySPI_Thread (void const *argument) {
+	osEvent evt;
+	char tab[22+16];
+	int i, nb_led;
+	
+	for (i=0;i<4;i++){
+		tab[i] = 0;
+	}
+	
+	// 4 LED bleues
+		for (nb_led = 0; nb_led <4;nb_led++){
+			tab[4+nb_led*4]=0xff;
+			tab[5+nb_led*4]=0xff;
+			tab[6+nb_led*4]=0x00;
+			tab[7+nb_led*4]=0x00;
+			}
+
+		// 4 LED rouges
+		for (nb_led = 0; nb_led <4;nb_led++){
+			tab[20+nb_led*4]=0xff;
+			tab[21+nb_led*4]=0x00;
+			tab[22+nb_led*4]=0x00;
+			tab[23+nb_led*4]=0xff;
+			}
+	
+		// end
+		tab[36] = 0;
+		tab[37] = 0;
+		
+	
+  while (1) {
+		
+		Driver_SPI1.Send(tab,38);
+    evt = osSignalWait(0x01, osWaitForever);	// sommeil fin emission
+		
+		osDelay(1000);
+			
+			
+  }
+}
+
+
 
 /* Private functions ---------------------------------------------------------*/
 /**
@@ -95,7 +176,7 @@ static void Error_Handler(void);
   */
 int main(void)
 {
-
+		
   /* STM32F4xx HAL library initialization:
        - Configure the Flash prefetch, Flash preread and Buffer caches
        - Systick timer is configured by default as source of time base, but user 
@@ -106,7 +187,7 @@ int main(void)
        - Low Level Initialization
      */
   HAL_Init();
-
+	
   /* Configure the system clock to 168 MHz */
   SystemClock_Config();
   SystemCoreClockUpdate();
@@ -114,26 +195,22 @@ int main(void)
   /* Add your application code here
      */
 	LED_Initialize();
-	
-
+	Init_SPI();
+	NVIC_SetPriority(SPI1_IRQn,2);
 #ifdef RTE_CMSIS_RTOS2	// A commenter si utilisation RTOS
   /* Initialize CMSIS-RTOS2 */
   osKernelInitialize ();
 
-  /* Create thread functions that start executing, 
-  Example: osThreadNew(app_main, NULL, NULL); */
-
-  /* Start thread execution */
+  /* Create thread functions that start executing, */
+	tid_mySPI_Thread = osThreadCreate (osThread(mySPI_Thread), NULL);
+  
+	/* Start thread execution */
   osKernelStart();
 #endif
+	
+	
+	osDelay(osWaitForever);
 
-  /* Infinite loop */
-  while (1)
-  {
-		LED_On (1);
-		LED_On (2);
-		LED_On (3);
-  }
 }
 
 /**
